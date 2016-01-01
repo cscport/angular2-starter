c57efeb08b713755c3e60c5c8bea394f901a5e4b
cscport <cscport@gmail.com>
2016-01-01 11:47:11 -0500

    Initial seed

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #Angular2 Starter
+
+Initially inspired by https://angular.io/docs/ts/latest/quickstart.html
---
+++ src/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+//import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+@Component({
+  selector: 'app',
+  template: '<h1>Angular2 Starter App</h1>'
+})
+
+//@RouteConfig([])
+
+export class AppComponent { }
